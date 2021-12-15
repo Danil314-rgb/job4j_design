f630c5e39666f2a7c251cac026732328c25614b4
Danil <totem7h@gmail.com>
2021-12-15 21:07:57 +0400

    2. Создать контейнер на базе связанного списка [#159 #239926]

--- src/main/java/ru/job4j/linkedlist/SimpleLinkedList.java
+++ src/main/java/ru/job4j/linkedlist/SimpleLinkedList.java
@@ -2,7 +2,9 @@ package ru.job4j.linkedlist;
 
 import org.w3c.dom.Node;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class SimpleLinkedList<E> implements List<E> {
@@ -10,29 +12,22 @@ public class SimpleLinkedList<E> implements List<E> {
     private static class Node<E> {
         E item;
         Node<E> next;
-        Node<E> prev;
 
-        Node(Node<E> prev, E element, Node<E> next) {
+        Node(E element, Node<E> next) {
             this.item = element;
             this.next = next;
-            this.prev = prev;
-        }
-
-        public Node<E> getNext() {
-            return next;
         }
     }
 
-    transient  int size = 0;
+    transient int size = 0;
     transient Node<E> first;
     transient Node<E> last;
-
     private int modCount = 0;
 
     @Override
     public void add(E value) {
         final Node<E> l = last;
-        final Node<E> newNode = new Node<>(l, value, null);
+        final Node<E> newNode = new Node<>(value, null);
         last = newNode;
         if (l == null) {
             first = newNode;
@@ -49,14 +44,35 @@ public class SimpleLinkedList<E> implements List<E> {
         Node<E> pref = first;
         E res = pref.item;
         for (int i = 0; i <= index; i++) {
-             res = pref.next.item;
-            /*return pref.next.item;*/
+            res = pref.item;
+            pref = pref.next;
         }
         return res;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            SimpleLinkedList.Node<E> res = first;
+            final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return res != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E result = res.item;
+                res = res.next;
+                return result;
+            }
+        };
     }
 }

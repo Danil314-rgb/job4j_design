fe6f343e8d2bdd970d5a480171b4a897c6df6008
Danil <totem7h@gmail.com>
2021-12-17 16:53:37 +0400

    5. Очередь на двух стеках [#160 #241103]

--- src/main/java/ru/job4j/linkedlist/SimpleQueue.java
+++ src/main/java/ru/job4j/linkedlist/SimpleQueue.java
@@ -11,45 +11,25 @@ public class SimpleQueue<T> {
     private int outCount = 0;
     private T res = null;
 
-    /*должен возвращать первое значение и удалять его из коллекции*/
     public T poll() {
         if (inCount == 0 && outCount == 0) {
             throw new NoSuchElementException();
         }
-        if (inCount == 0) {
-            revers();
-        }
         if (outCount == 0) {
-            while (inCount > 1) {
+            while (inCount != 0) {
                 res = in.pop();
                 out.push(res);
                 inCount--;
                 outCount++;
             }
-            if (inCount == 1) {
-                res = in.pop();
-                inCount--;
-            }
         }
+        res = out.pop();
+        outCount--;
         return res;
     }
 
-    /*помещает значение в конец*/
     public void push(T value) {
-        if (outCount != 0) {
-            revers();
-        }
         in.push(value);
         inCount++;
-    }
-
-    /*возвращает значения из out в in*/
-    public void revers() {
-        while (outCount != 0) {
-            res = out.pop();
-            in.push(res);
-            outCount--;
-            inCount++;
-        }
     }
 }

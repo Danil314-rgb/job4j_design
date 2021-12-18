d7bdd59744d57b0688694b83396575d463e0db5f
Danil <totem7h@gmail.com>
2021-12-18 17:08:28 +0400

    7. ListIterator [#350217 #241394]

--- src/main/java/ru/job4j/linkedlist/ListUtils.java
+++ src/main/java/ru/job4j/linkedlist/ListUtils.java
@@ -8,52 +8,52 @@ public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> i = list.listIterator();
-        while (i.hasNext()) {
-            if (i.nextIndex() == index) {
-                i.add(value);
+        ListIterator<T> iterator = list.listIterator(index);
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.add(value);
                 break;
             }
-            i.next();
+            iterator.next();
         }
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> i = list.listIterator();
-        while (i.hasNext()) {
-            if (i.nextIndex() == index) {
-                i.next();
-                i.add(value);
+        ListIterator<T> iterator = list.listIterator(index);
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.next();
+                iterator.add(value);
                 break;
             }
-            i.next();
+            iterator.next();
         }
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
-        ListIterator<T> i = list.listIterator();
-        while (i.hasNext()) {
-            if (filter.test(i.next())) {
-                i.remove();
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.remove();
             }
         }
     }
 
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
-        ListIterator<T> i = list.listIterator();
-        while (i.hasNext()) {
-            if (filter.test(i.next())) {
-                i.set(value);
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.set(value);
             }
         }
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        ListIterator<T> l = list.listIterator();
-        while (l.hasNext()) {
-            if (elements.contains(l.next())) {
-                l.remove();
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (elements.contains(iterator.next())) {
+                iterator.remove();
             }
         }
     }
--- src/test/java/ru/job4j/linkedlist/ListUtilsTest.java
+++ src/test/java/ru/job4j/linkedlist/ListUtilsTest.java
@@ -34,7 +34,7 @@ public class ListUtilsTest {
     @Test
     public void whenRemoveIf() {
         List<Integer> input = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 4, 5, 6, 7));
-        ListUtils.removeIf(input, i -> i % 2 == 0);
+        ListUtils.removeIf(input, value -> value % 2 == 0);
         assertThat(input, is(Arrays.asList(1, 3, 5, 7)));
     }
 
@@ -42,21 +42,21 @@ public class ListUtilsTest {
     public void whenRemoveIf1() {
         List<Integer> input = new ArrayList<>(Arrays.asList(2, 2, 2, 6, 8, 12, 2));
         List<Integer> output = new ArrayList<>();
-        ListUtils.removeIf(input, i -> i % 2 == 0);
+        ListUtils.removeIf(input, value -> value % 2 == 0);
         assertThat(output, is(input));
     }
 
     @Test
     public void whenReplaceIt() {
         List<Integer> input = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6, 7));
-        ListUtils.replaceIf(input, i -> i % 2 == 0, -1);
+        ListUtils.replaceIf(input, value -> value % 2 == 0, -1);
         assertThat(input, is(Arrays.asList(-1, 3, -1, 5, -1, 7)));
     }
 
     @Test
     public void whenReplaceIt1() {
         List<Integer> input = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9));
-        ListUtils.replaceIf(input, i -> i % 2 == 0, -1);
+        ListUtils.replaceIf(input, value -> value % 2 == 0, -1);
         assertThat(input, is(Arrays.asList(1, 3, 5, 7, 9)));
     }
 

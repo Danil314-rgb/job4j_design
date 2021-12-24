79483c248ff1a6578e0c1843e27211116f065fd9
Danil <totem7h@gmail.com>
2021-12-24 13:24:04 +0400

    2. Добавить метод boolean isBinary до изменения

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,9 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class User {
 
@@ -18,23 +15,58 @@ public class User {
     }
 
     public static void main(String[] args) {
-        User userOne = new User("Tom", 1, new GregorianCalendar(1980, 1, 25));
-        User userTwo = new User("Tom", 1, new GregorianCalendar(1980, 1, 25));
+
+        System.out.println();
+
+        /*Calendar birth = new GregorianCalendar(1980, 1, 25);
+
+        User userOne = new User("Tom", 1, birth);
+        User userTwo = new User("Tom", 1, birth);
 
         Map<User, Object> map = new HashMap<>();
         map.put(userOne, new Object());
-        map.put(userTwo, new Object());
+        map.put(userTwo, new Object());*/
 
-        for (Map.Entry<User, Object> entry : map.entrySet()) {
+        /*for (Map.Entry<User, Object> entry : map.entrySet()) {
             User key = entry.getKey();
             Object value = entry.getValue();
             System.out.println(key + " = " + value);
-        }
+            System.out.println("hashCode = " + key.hashCode());
+        }*/
+        /*System.out.println("KING".hashCode());
+        System.out.println(hash("KING"));
+        int i = (33 + 111) & 34;
+        System.out.println(i);
+        int a = (10 - 2) & 32;
+        System.out.println(a);*/
+
     }
+
+    /*static final int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }*/
 
     @Override
     public String toString() {
         return "User{" + "name='" + name + '\'' + ", children=" + children + ", birthday=" + birthday + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
 

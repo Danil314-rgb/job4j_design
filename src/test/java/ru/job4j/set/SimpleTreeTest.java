ebb2168c5cb9739b7eda1e49096053d1178dc8cc
Danil <totem7h@gmail.com>
2021-12-24 11:58:35 +0400

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/test/java/ru/job4j/set/SimpleTreeTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.set;
+
+import org.junit.Test;
+import tree.SimpleTree;
+import tree.Tree;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleTreeTest {
+
+    @Test
+    public void add() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.findBy(6).isPresent(), is(true));
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test
+    public void whenChildExistOnLeafThenNotAdd() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertFalse(tree.add(2, 6));
+    }
+}

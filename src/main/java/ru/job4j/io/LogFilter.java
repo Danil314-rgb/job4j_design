745be1b34fe2429013f17a8a04bb4857e13d8520
Danil <totem7h@gmail.com>
2022-01-04 15:09:57 +0400

    0.3. BufferedReader. [#252489 #246829]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,15 +1,14 @@
 package ru.job4j.io;
 
-import ru.job4j.linkedlist.List;
-import ru.job4j.linkedlist.SimpleLinkedList;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LogFilter {
 
     public static List<String> filter(String file) {
-        List<String> list = new SimpleLinkedList<>();
+        List<String> list = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             in.lines().filter(s -> s.contains(" 404 ")).forEach(list::add);
         } catch (Exception e) {
@@ -21,6 +20,5 @@ public class LogFilter {
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
         log.forEach(System.out::println);
-        System.out.println(log);
     }
 }

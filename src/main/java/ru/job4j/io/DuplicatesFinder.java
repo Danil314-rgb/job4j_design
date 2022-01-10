517d28c23029ff3bac2b5ee5b1bc98b272fbef2e
Danil <totem7h@gmail.com>
2022-01-10 18:19:15 +0400

    4.2. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/DuplicatesFinder.java
@@ -0,0 +1,15 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class DuplicatesFinder {
+
+    public static void main(String[] args) throws IOException {
+
+        DuplicateVisitor duplicateVisitor = new DuplicateVisitor();
+        Files.walkFileTree(Path.of("./"), duplicateVisitor);
+        duplicateVisitor.getFiles().forEach(System.out::println);
+    }
+}

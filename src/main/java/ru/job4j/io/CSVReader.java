32eb883deb1be9ac092c3fe165b9f2bcf88e0302
Danil <totem7h@gmail.com>
2022-01-14 18:47:04 +0400

    7. Scanner [#504791 #250303]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,9 +1,35 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.*;
 
 public class CSVReader {
+
+    public static void main(String[] args) throws Exception {
+        /*File file = new File("./resources/source.csv");
+        File target = new File("./resources/target.csv");*/
+
+        File fileArgs = new File(args[0]);
+        File targetArgs = new File(args[1]);
+        if (args.length != 2) {
+            throw new IllegalArgumentException(
+                    "Ошибка. Нужно передать ровно два параметра!");
+        }
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        Files.writeString(fileArgs.toPath(), data);
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + fileArgs.getAbsolutePath(), "-delimiter=;",
+                "-out=" + targetArgs.getAbsolutePath(), "-filter=name,age"
+        });
+        handle(argsName, targetArgs.getAbsolutePath(), fileArgs.getAbsolutePath());
+    }
 
     public static void handle(ArgsName argsName, String target, String file) throws Exception {
         Scanner scanner = new Scanner(new FileReader(file));

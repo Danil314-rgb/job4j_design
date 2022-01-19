3404694084339bb7ae09afe48bc255ba223ca7e5
Danil <totem7h@gmail.com>
2022-01-19 13:56:04 +0400

    0. Что такое база данных. [#6860 #252300]

---
+++ sql/0. Что такое база данных/0.sql
@@ -0,0 +1,16 @@
+create table employee (
+	id serial primary key,
+	name varchar(255),
+	age int,
+	salary float,
+	position text		
+);
+
+insert into employee  
+ (name, age, salary, position) 
+ values
+ ('Bob','35', 27.000, 'Менеджер');
+ 
+ update employee set name = 'Tom', age = 32
+
+delete from employee

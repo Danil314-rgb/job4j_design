3552c411e2da546243fe9916480243de41e8f012
Danil <totem7h@gmail.com>
2022-01-23 16:12:31 +0400

    2. Группировка и агрегатные функции [#373618 #253094]

--- sql/0. Select/2.group.sql
+++ sql/0. Select/2.group.sql
@@ -1,8 +1,55 @@
+create table devices(
+    id serial primary key,
+    name varchar(255),
+    price float
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table devices_people(
+    id serial primary key,
+    device_id int references devices(id),
+    people_id int references people(id)
+);
+
+insert into people (name)
+values ('Tom');
+
+insert into people (name)
+values ('Bob');
+
+insert into devices (name, price)
+values ('Samsung S25', 50000);
+
+insert into devices (name, price)
+values ('Samsung S21', 45000);
+
+insert into devices (name, price)
+values ('Iphone 12', 80000);
+
+insert into devices (name, price)
+values ('Iphone 12', 25000);
+
+insert into devices_people (device_id, people_id)
+values (1, 1);
+
+insert into devices_people (device_id, people_id)
+values (2, 1);
+
+insert into devices_people (device_id, people_id)
+values (3, 2);
+
+insert into devices_people (device_id, people_id)
+values (4, 2);
+
 select avg(price)
 from devices;
 
-select avg(price), p.name
-from devices d
+select p.name, avg(d.price)
+from devices as d
 join people p
 on d.id = p.id
 group by p.name;
@@ -13,3 +60,7 @@ join people p
 on d.id = p.id
 group by p.name
 having avg(price) > 50.000;
+
+select * from devices;
+select * from people;
+select * from devices_people;

5b78d4082d225430d3e0ade637f0595a5ac43c23
Danil <totem7h@gmail.com>
2022-01-25 18:42:04 +0400

    0.2. PrepareStatement [#379307]

---
+++ src/main/java/ru/job4j/sql/ImportDB.java
@@ -0,0 +1,73 @@
+package ru.job4j.sql;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class ImportDB {
+
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        Scanner scanner = new Scanner(new FileReader(dump));
+        while (scanner.hasNext()) {
+            String text = scanner.nextLine();
+            String[] strings = text.split(";");
+            users.add(new User(strings[0], strings[1]));
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("url"),
+                cfg.getProperty("login"),
+                cfg.getProperty("password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement statement = cnt.prepareStatement(
+                        "insert into users(name, email) values (?, ?)")) {
+                    statement.setString(1, user.name);
+                    statement.setString(2, user.email);
+                    statement.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("./resources/app.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "./resources/dump.txt");
+        db.save(db.load());
+    }
+}
